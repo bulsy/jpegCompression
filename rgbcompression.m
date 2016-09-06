ba49c3fb26d0eaa29b0dbd553a5af3d4e75994e3
Bilge <bilgus35@gmail.com>
2016-09-06 15:29:09 +0200

    Delete rgbcompression.m

--- rgbcompression.m
+++
@@ -1,9 +0,0 @@
-function [output]=rgbcompression(f,quality)
-[F4]= jpegCompression(f,quality);
-F4=uint8(F4);
-
-F4=ycbcr2rgb(F4);
-output=F4;
-figure;
-subplot(1,2,1),imshow(f);
-subplot(1,2,2),imshow(F4);
